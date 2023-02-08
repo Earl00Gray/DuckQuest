71a38962ea86f25dd80cae235b9de4de6b553047
Duck Hunter <hduck.forever@gmail.com>
2023-02-08 22:50:41 +0300

    [add] GameState implementation

--- src/server/engine/game_state.py
+++ src/server/engine/game_state.py
@@ -1,11 +1,74 @@
-from game_state_item import History, Context, Character, Inventory
+from typing import List
+
+from game_state_item import History, Context, Character, Inventory, HistoryItem
+from common.I_Result import I_Result, ResultType
+from action_result import LootResult, ExpereanceResult, LootResultType
 
 class GameState:
-    def __init__(self) -> None:
+    def __init__(self, story_config_path: str = "") -> None:
+        self.__m_story_config_path: str = story_config_path
         self.__m_history: History
         self.__m_context: Context
         self.__m_character: Character
         self.__m_inventory: Inventory
   
-    def storeStep(self, step_id: int, result: int, res_value: int) -> None:
-        pass
+    @property
+    def story_config_path(self) -> str:
+        return self.__m_story_config_path
+  
+    @property
+    def history(self) -> History:
+        return self.__m_history
+  
+    @property
+    def context(self) -> Context:
+        return self.__m_context
+  
+    @property
+    def character(self) -> Character:
+        return self.__m_character
+  
+    @property
+    def inventory(self) -> Inventory:
+        return self.__m_inventory
+
+    def storeStep(self, step_id: int, result_lst: List[I_Result]) -> None:
+        if step_id is None or result_lst is None:
+            return
+
+        self.__applyStep(HistoryItem(step_id, result_lst))
+
+        for result_item in result_lst:
+            result_item_type: ResultType = result_item.getType()
+           
+            if result_item_type is ResultType.LOOT:
+                self.__applyLoot(result_item)
+            elif result_item_type is ResultType.EXPEREANCE:
+                self.__applyExpereance(result_item)
+
+    def __applyStep(self, history_item: HistoryItem) -> None:
+        if history_item is None:
+            return
+
+        self.__m_history.appendStep(history_item)
+
+    def __applyLoot(self, result_item: LootResult) -> None:
+        if result_item is None:
+            return
+            
+        if result_item.loot_res_type is LootResultType.ADD:
+            self.__m_inventory.addLoot(result_item.loot_id)
+        elif result_item.loot_res_type is LootResultType.RM:
+            self.__m_inventory.rmLoot(result_item.loot_id)
+
+    def __applyExpereance(self, result_item: ExpereanceResult) -> None:
+        if result_item is None:
+            return
+
+        if result_item.var.startswith(f"{Character.prefix}."):
+            self.__m_character.applyOperation(result_item.var, result_item.value, result_item.operation)
+        elif result_item.var.startswith(f"{Context.prefix}."):
+            self.__m_context.applyOperation(result_item.var, result_item.value, result_item.operation)
+        
+
+    
--- src/server/engine/game_state_item.py
+++ src/server/engine/game_state_item.py
@@ -1,56 +1,104 @@
 from typing import Dict, List, Any
+from dataclasses import dataclass, field
+
 from common.i_game_state_item import I_GameStateItem, GameStateItemType
 from common.I_Result import I_Result
+from action_result import ExpereanceOperatorType
+
+@dataclass
+class HistoryItem:
+    step_id: int = field(default = -1)
+    result_lst: List[I_Result] = field(default_factory = List[I_Result])
 
 class History(I_GameStateItem):
     def __init__(self) -> None:
         super().__init__(GameStateItemType.HISTORY)
-        self.__m_step_dict: Dict[int, List[I_Result]]
+        self.__m_step_lst: List[HistoryItem]
   
-    def appendStep(self, step_res_lst: List[I_Result]) -> None:
-        pass
+    def appendStep(self, history_item: HistoryItem) -> None:
+        self.__m_step_lst.append(history_item)
 
-    def getStep(step_id: int) -> List[I_Result]:
-        pass
+    def getStep(self, index: int) -> HistoryItem:
+        if self.__m_step_lst.count >= index:
+            return self.__m_step_lst[index]
+        
+        return None
 
 class Context(I_GameStateItem): # for save expereance and relationship
+    prefix = "Context"
+
     def __init__(self) -> None:
         super().__init__(GameStateItemType.CONTEXT)
         self.__m_var_dict: Dict[str, Any]
   
-    def getVar(var_name: str) -> Any:
-        pass
+    def getVar(self, var_name: str) -> Any:
+        if var_name in self.__m_var_dict:
+            return self.__m_var_dict[var_name]
 
-    def setVar(var_name: str, var_value: Any) -> None:
-        pass
+        return None
+
+    def setVar(self, var_name: str, var_value: Any) -> None:
+        self.__m_var_dict[var_name] = var_value
+
+    def applyOperation(self, var_name: str, var_value: Any, operation: ExpereanceOperatorType) -> None:
+        if operation is ExpereanceOperatorType.ADD:
+            if var_name in self.__m_var_dict:
+                self.__m_var_dict[var_name] += var_value
+            else:
+                self.__m_var_dict[var_name] = var_value
+
+        elif operation is ExpereanceOperatorType.SET:
+            self.__m_var_dict[var_name] = var_value
+
+        elif operation is ExpereanceOperatorType.SUB:
+            if var_name in self.__m_var_dict:
+                self.__m_var_dict[var_name] -= var_value
 
 class Character(I_GameStateItem): #for save character state
+    prefix = "Character"
+
     def __init__(self) -> None:
         super().__init__(GameStateItemType.CHARACTER)
         self.__m_var_dict: Dict[str, Any]
   
-    def getVar(var_name: str) -> Any:
-        pass
+    def getVar(self, var_name: str) -> Any:
+        if var_name in self.__m_var_dict:
+            return self.__m_var_dict[var_name]
 
-    def setVar(var_name: str, var_value: Any) -> None:
-        pass
+    def setVar(self, var_name: str, var_value: Any) -> None:
+        self.__m_var_dict[var_name] = var_value
 
-class InventoryItem: #TODO: blob
-    pass
+    def applyOperation(self, var_name: str, var_value: Any, operation: ExpereanceOperatorType) -> None:
+        if operation is ExpereanceOperatorType.ADD:
+            if var_name in self.__m_var_dict:
+                self.__m_var_dict[var_name] += var_value
+            else:
+                self.__m_var_dict[var_name] = var_value
+
+        elif operation is ExpereanceOperatorType.SET:
+            self.__m_var_dict[var_name] = var_value
+
+        elif operation is ExpereanceOperatorType.SUB:
+            if var_name in self.__m_var_dict:
+                self.__m_var_dict[var_name] -= var_value
 
 class Inventory(I_GameStateItem):
     def __init__(self) -> None:
         super().__init__(GameStateItemType.INVENTORY)
-        self.__m_loot_dict: Dict[int, InventoryItem]
+        self.__m_loot_lst: List[int]
 
-        def getLoot(loot_id: int) -> InventoryItem: #TODO: think about it
-            pass
+    def getLoot(self, index: int) -> int: ##TODO: think about it
+        if self.__m_loot_lst.count >= index:
+            return self.__m_loot_lst[index]
 
-        def getLootLst(loot_id: int) -> List[InventoryItem]:
-            pass
+        return None
 
-        def addLoot(loot_id: int) -> None:
-            pass
+    def getLootLst(self) -> List[int]:
+        return self.__m_loot_lst
 
-        def rmLoot(loot_id: int) -> None:
-            pass
+    def addLoot(self, loot_id: int) -> None:
+        self.__m_loot_lst.append(loot_id)
+
+    def rmLoot(self, loot_id: int) -> None:
+        if loot_id in self.__m_loot_lst:
+            self.__m_loot_lst.remove(loot_id)
