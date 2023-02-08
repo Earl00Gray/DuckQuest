e5119927686c9cc6f0b7ff13be90e9dcc72835ee
Duck Hunter <hduck.forever@gmail.com>
2023-02-08 22:48:12 +0300

    [add] loot result type for adding or removing item

--- src/server/engine/action_result.py
+++ src/server/engine/action_result.py
@@ -11,24 +11,34 @@ class GoToResult(I_Result):
     def next_step_id(self) -> int:
         return self.__m_next_step_id
 
+class LootResultType(StrEnum):
+    UNKNOWN = "UNKNOWN"
+    ADD = "add"
+    RM = "rm"
+
 class LootResult(I_Result):
-    def __init__(self, id: str) -> None:
+    def __init__(self, id: str, loot_res_type: LootResultType) -> None:
         super().__init__(ResultType.LOOT)
        
         self.__m_loot_id = id
+        self.__m_loot_res_type = loot_res_type
 
     @property
     def loot_id(self) -> str:
         return self.__m_loot_id
+
+    @property
+    def loot_res_type(self) -> LootResultType:
+        return self.__m_loot_res_type
     
-class OperatorType(StrEnum):
+class ExpereanceOperatorType(StrEnum):
     UNKNOWN = "UNKNOWN"
     ADD = "add"
     SUB = "sub"
     SET = "set"
 
 class ExpereanceResult(I_Result):
-    def __init__(self, var_name: str, operator: OperatorType, value: float) -> None:
+    def __init__(self, var_name: str, operator: ExpereanceOperatorType, value: float) -> None:
         super().__init__(ResultType.EXPEREANCE)
        
         self.__m_var = var_name
@@ -40,7 +50,7 @@ class ExpereanceResult(I_Result):
         return self.__m_var
     
     @property
-    def operation(self) -> OperatorType:
+    def operation(self) -> ExpereanceOperatorType:
         return self.__m_operation
     
     @property
